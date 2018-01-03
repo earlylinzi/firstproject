9c4215982aaebb42b97c4c707823e1940defa5fd
80002523 <elin.li@foxmail.com>
2018-01-03 16:53:09 +0800

    [add files]

--- src/main/java/com/myutil/practice/GetPrivateStatic.java
+++ src/main/java/com/myutil/practice/GetPrivateStatic.java
@@ -3,5 +3,22 @@ package com.myutil.practice;
 
 public class GetPrivateStatic {
     public static void main(String[] args) {
+//        String str = "zhende真的😊是的";
+        String str = "zhende真的是的";
+//        String pattern = "\\w";
+//
+//        Pattern r = Pattern.compile(pattern);
+//        Matcher m = r.matcher(str);
+//        System.out.println(m.matches());
+
+        for (int i = 0; i < 100; i++) {
+            char a = 0xFFF1 + 0xc;
+
+
+        System.out.println(String.valueOf(a));
+        }
+//        boolean b = EmojiFilter.containsEmoji(str);
+//        System.out.println(b);
+//
     }
 }
