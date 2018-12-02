f8e8a4de4e834d5a1d93a526de1013b2ce410552
80002523 <80002523@sf-express.com>
2018-12-02 22:41:26 +0800

    test

---
+++ src/main/java/practice/Test.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int i = 5;
+        byte b = 2;
+        double d = 3.6;
+
+        System.out.println(i + d);
+
+    }
+}
